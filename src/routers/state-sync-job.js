029a95dc08e5e84b59f7b53f83327ea97092bc47
Yilin Ma <1135485226@qq.com>
2020-10-18 20:37:58 -0700

    Test ShipEngine API

--- src/routers/state-sync-job.js
+++ src/routers/state-sync-job.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import axios from 'axios';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).send('Hello state-sync router!');
+router.get('/', async (req, res) => {
+  const config = {
+    method: 'get',
+    url:
+      'http://api.shipengine.com/v1/tracking?carrier_code=usps&tracking_number=9374889676091289203070',
+    headers: {
+      'API-KEY': 'TEST_fF1MGE2gp9GFuBAKcmyKDZ9gWoYpGRAbjGKo7jpTiPk',
+    },
+  };
+
+  try {
+    const response = await axios(config);
+    res.status(200).send(response.data);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 export default router;
